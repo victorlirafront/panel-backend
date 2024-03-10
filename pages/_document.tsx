1ce97feac3602bfad41a067f5cf70332581edc6b
VictorLira-DEV <victorliradeveloper@gmail.com>
2024-03-10 11:54:19 -0300

    refactor: improve _document.tsx file

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,19 +1,23 @@
-import Document from 'next/document';
+import Document, { DocumentContext, DocumentInitialProps } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
+import React from 'react';
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx: any) {
+  static async getInitialProps(
+    ctx: DocumentContext,
+  ): Promise<DocumentInitialProps> {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
 
     try {
       ctx.renderPage = () =>
         originalRenderPage({
-          enhanceApp: (App: any) => (props: any) =>
-            sheet.collectStyles(<App {...props} />),
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
         });
 
-      const initialProps = await Document.getInitialProps(ctx);
+      const initialProps: DocumentInitialProps = await Document.getInitialProps(
+        ctx,
+      );
       return {
         ...initialProps,
         styles: (
