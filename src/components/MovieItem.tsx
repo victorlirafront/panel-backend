e06e10ae48abeb1d57611b3aa2eb0df4ac15712c
VictorLira-DEV <victorliradeveloper@gmail.com>
2024-03-09 22:45:34 -0300

    feat: add vote fomatter function

--- src/components/MovieItem.tsx
+++ src/components/MovieItem.tsx
@@ -6,14 +6,21 @@ import {
   StyledMovieButton,
 } from './styles/MovieItem.styled';
 import { useEffect } from 'react';
-import { useRouter } from 'next/router';
 import { IMovieItem } from '../interface/interfaces';
 
 import Aos from 'aos';
 import 'aos/dist/aos.css';
 
+const voteFormatter = function (numero: number) {
+  if (typeof numero !== 'number') {
+    throw new Error('O argumento deve ser uma string contendo um número.');
+  }
+
+  const numeroAntesDoPonto = String(numero).split('.')[0];
+  return parseInt(numeroAntesDoPonto, 10);
+};
+
 function MovieItem(props: IMovieItem) {
-  const router = useRouter();
   useEffect(() => {
     Aos.init();
   }, []);
@@ -33,7 +40,7 @@ function MovieItem(props: IMovieItem) {
       />
       <StyledMovieInfo>
         <h3>{props.title}</h3>
-        <span className={props.average}> {props.vote} </span>
+        <span className={props.average}> {voteFormatter(props.vote)} </span>
       </StyledMovieInfo>
       <StyledMovieOverview className="movie-overview">
         <StyledMovieButton onClick={e => props.onClick(e)}>
