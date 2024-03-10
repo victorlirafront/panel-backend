7d2365994db53a3137e82fc334971d2005ff0799
VictorLira-DEV <victorliradeveloper@gmail.com>
2024-03-09 22:34:16 -0300

    chore: prettier config

--- src/components/ui/NoMovieFound.tsx
+++ src/components/ui/NoMovieFound.tsx
@@ -1,15 +1,14 @@
-import StyledNoMovieFound from '../styles/NoMovieFound'
+import StyledNoMovieFound from '../styles/NoMovieFound';
 
-function NoMovieFound(){
-    return (
-        <StyledNoMovieFound>
-            <div>
-                <img src="./404.svg" alt="no movie was found" />
-                <h1>No Movie Was Found</h1>
-            </div>
-        </StyledNoMovieFound>
-    )
+function NoMovieFound() {
+  return (
+    <StyledNoMovieFound>
+      <div>
+        <img src="./404.svg" alt="no movie was found" />
+        <h1>No Movie Was Found</h1>
+      </div>
+    </StyledNoMovieFound>
+  );
 }
 
-
-export default NoMovieFound;
+export default NoMovieFound;s
