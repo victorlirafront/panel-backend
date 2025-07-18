08e05528c341d70f80f1990c0931e852627b56b2
victorlirafront <victorliradeveloper@gmail.com>
2025-07-18 16:51:19 -0300

    chore: comment testing file

--- src/test/java/com/ecommerce/EcommerceApplicationTests.java
+++ src/test/java/com/ecommerce/EcommerceApplicationTests.java
@@ -1,13 +1,13 @@
-package com.ecommerce;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class EcommerceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.ecommerce;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class EcommerceApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
